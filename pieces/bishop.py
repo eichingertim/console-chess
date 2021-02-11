266d0e878e2a1be5b83b72d981962b0d29dbc57c
Tim Eichinger <privat.timeichinger@gmail.com>
2021-02-11 12:22:32 +0100

    comments and slight code improvements

--- pieces/bishop.py
+++ pieces/bishop.py
@@ -2,6 +2,7 @@ from .base_piece import BasePiece
 from .piece_type import PieceType
 from .empty import Empty
 from .color import Color
+import operator
 
 class Bishop(BasePiece):
 
@@ -18,11 +19,13 @@ class Bishop(BasePiece):
 
         cur_row, cur_col = board.get_position(self) 
 
-        # Checks if it's a diagonal movement
-        if (cur_col, cur_row) != (dest_row, dest_col) or \
+        # Checks if destination position is current position 
+        # or it's not a diagonal movement
+        if (cur_col, cur_row) == (dest_row, dest_col) or \
              abs(cur_col - dest_col) != abs(cur_row - dest_row):
             return False
 
+        # calculates the direction we should check for the move
         direction = (1 if cur_row - dest_row < 0 else - 1, \
             1 if cur_col - dest_col < 0 else -1)
 
@@ -31,10 +34,12 @@ class Bishop(BasePiece):
                 (dest_row, dest_col), (cur_row, cur_col), board)
         
 
+    # goes through all fields in the given direction and checks if 
+    # the piece can move to this point
     def check_move_for_direction(self, dir, destPos, curPos, board):
         for distance in range(1, 8):
             move = (distance * dir[0], distance*dir[1])
-            new_pos = (curPos[0] + move[0], curPos[1] + move[1])
+            new_pos = tuple(map(operator.add, curPos, move))
 
             if board.is_valid_pos(new_pos):
                 piece_at_pos = board.board[new_pos[0]][new_pos[1]]
@@ -46,10 +51,7 @@ class Bishop(BasePiece):
                     if piece_at_pos.color == self.color:
                         return False
                     else:
-                        if (new_pos == destPos):
-                            return True
-                        else:
-                            return False
+                        return new_pos == destPos
             else:
                 return False
         

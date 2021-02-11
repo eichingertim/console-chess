6dd23ca569ff35ba5135d62f2eb7484df22b01cf
Tim Eichinger <privat.timeichinger@gmail.com>
2021-02-11 16:09:24 +0100

    implemented king and knight move checking

--- pieces/king.py
+++ pieces/king.py
@@ -1,6 +1,7 @@
 from .base_piece import BasePiece
 from .piece_type import PieceType
 from .color import Color
+from .empty import Empty
 
 class King(BasePiece):
 
@@ -12,3 +13,21 @@ class King(BasePiece):
             return "\u265A"
         else:
             return "\u2654"
+
+    def can_move(self, board, dest_row, dest_col):
+
+        cur_row, cur_col = board.get_position(self) 
+
+        # Checks if destination position is current position
+        if (cur_col, cur_row) == (dest_row, dest_col):
+            return False
+
+        # Check the move
+        if (abs(dest_row - cur_row) <= 1 or abs(dest_col - cur_col) <= 1):
+            piece_at_pos = board.board[dest_row][dest_col]
+            if isinstance(piece_at_pos, Empty):
+                return True
+            else:
+                return piece_at_pos.get_color() != self.get_color()
+        
+        return False
--- pieces/knight.py
+++ pieces/knight.py
@@ -1,6 +1,7 @@
 from .base_piece import BasePiece
 from .piece_type import PieceType
 from .color import Color
+from .empty import Empty
 
 class Knight(BasePiece):
 
@@ -12,3 +13,26 @@ class Knight(BasePiece):
             return "\u265E"
         else:
             return "\u2658"
+
+    def can_move(self, board, dest_row, dest_col):
+
+        cur_row, cur_col = board.get_position(self) 
+
+        # Checks if destination position is current position
+        if (cur_col, cur_row) == (dest_row, dest_col):
+            return False
+
+        if self.valid_move(cur_row, cur_col, dest_row, dest_col):
+            piece_at_pos = board.board[dest_row][dest_col]
+            if isinstance(piece_at_pos, Empty):
+                return True
+            else:
+                return piece_at_pos.get_color() != self.get_color()
+
+        
+        return False
+        
+
+    def valid_move(self, cur_row, cur_col, dest_row, dest_col):
+        return (abs(cur_row - dest_row) == 2 and abs(cur_col - dest_col) == 1) \
+            or (abs(cur_row - dest_row) == 1 and abs(cur_col - dest_col) == 2)
